890496ffbdf11490a76199058fc5f54a79cdf806
adii <77727680+frozen-beak@users.noreply.github.com>
2024-12-22 19:23:55 +0530

    example usage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use http_rs::http::HTTP;
+use std::io::BufReader;
+
+use http_rs::http::{Request, Server};
 
 fn main() {
-    HTTP::new();
+    let server = Server::new("127.0.0.1:6969");
+
+    for stream in server.listen() {
+        let stream = stream.unwrap();
+        let buf = BufReader::new(stream);
+
+        if let Ok(req) = Request::new(buf) {
+            println!("{req:?}");
+        }
+    }
 }
